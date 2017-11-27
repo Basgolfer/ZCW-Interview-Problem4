306174bc39f649bfd655ef792fabf5217d841ef4
brian <brian@zipcoders-MacBook-Pro.local>
2017-11-27 17:14:06 -0500

    Done

--- src/main/java/io/zipcoder/Problem4.java
+++ src/main/java/io/zipcoder/Problem4.java
@@ -1,4 +1,52 @@
 package io.zipcoder;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Problem4 {
+
+    public boolean isPalindrome(String input) {
+        StringBuilder stringBuilder = new StringBuilder(input);
+        String reverse = stringBuilder.reverse().toString();
+        return input.equalsIgnoreCase(reverse);
+    }
+
+    public Integer findFrequencyOfACharacterInAString(String input, Character character) {
+        ArrayList<Character> arrayList = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            arrayList.add(input.charAt(i));
+        }
+        Integer countOfGivenCharacter = Collections.frequency(arrayList, character);
+//        if (countOfGivenCharacter >=1) {
+//            System.out.println("The frequency of " + character + " is " + countOfGivenCharacter);
+//        }
+        return countOfGivenCharacter;
+    }
+
+    public ArrayList<Integer> findFrequencyOfAllCharacters(String input) {
+        ArrayList<Integer> frequencyOfCharacter = new ArrayList<Integer>();
+        for (char i = 'a'; i <= 'z'; i++) {
+            frequencyOfCharacter.add(findFrequencyOfACharacterInAString(input, i));
+        }
+        return frequencyOfCharacter;
+    }
+
+    public String IsPalindromePermutation(String input) {
+        ArrayList<Integer> frequencyOfAllCharacters = findFrequencyOfAllCharacters(input);
+        Integer numberOfCharactersThatHaveAnOddPresence = 0;
+        for (int index = 0; index < frequencyOfAllCharacters.size(); index++) {
+            if (frequencyOfAllCharacters.get(index) % 2 == 1) {
+                numberOfCharactersThatHaveAnOddPresence++;
+            }
+        }
+        if (numberOfCharactersThatHaveAnOddPresence == 0 || numberOfCharactersThatHaveAnOddPresence == 1) {
+            return "YES";
+        }
+        return "NO";
+    }
+
+
+
+
 }
--- src/test/java/io/zipcoder/Problem4Test.java
+++ src/test/java/io/zipcoder/Problem4Test.java
@@ -1,4 +1,47 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem4Test {
+
+    @Test
+    public void isPalindromeTest_Pass() {
+        Assert.assertTrue(new Problem4().isPalindrome("aabbaa"));
+    }
+
+    @Test
+    public void findFrequencyOfACharacterInAStringTest() {
+        Integer expected = 3;
+        Integer actual = new Problem4().findFrequencyOfACharacterInAString("aaabbbb", 'a');
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findFrequencyOfAllCharactersTest() {
+        Integer expected = 26;
+        Integer actual = new Problem4().findFrequencyOfAllCharacters("aaabbbb").size();
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest_aaabbbb() {
+        String expected = "YES";
+        String actual = new Problem4().IsPalindromePermutation("aaabbbb");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest_cdefghmnopqrstuvw() {
+        String expected = "NO";
+        String actual = new Problem4().IsPalindromePermutation("cdefghmnopqrstuvw");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isPalindromePermutationTest_cdcdcdcdeeeef() {
+        String expected = "YES";
+        String actual = new Problem4().IsPalindromePermutation("cdcdcdcdeeeef");
+        Assert.assertEquals(expected, actual);
+    }
 }
